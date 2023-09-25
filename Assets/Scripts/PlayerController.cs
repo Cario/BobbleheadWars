815292ea6f9dfc15b4ede8c2c5c8ac6e746aa5c1
Muhammad Asjad <m.asjad.ca@gmail.com>
2023-09-24 20:40:51 -0400

    Ch 4 - Added Raycasting to turn Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody head;
+
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
@@ -32,5 +37,18 @@ public class PlayerController : MonoBehaviour
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            currentLookTarget = hit.point;
+        }
+
+        Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+        Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+
+        //Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
     }
 }
